4cdbaaadbc09a8e92442f2458d8efe56a43945c2
igor-paiva <igorbatistapaiva@outlook.com>
2022-08-29 00:10:27 -0300

    Create cuts queue page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement,
+  document.getElementById('root') as HTMLElement
 );
+
 root.render(<App />);
