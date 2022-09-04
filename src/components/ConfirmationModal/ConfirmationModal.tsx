63eda30b5b13ec4a7df6e781be0ac2099c71f5a0
igor-paiva <igorbatistapaiva@outlook.com>
2022-09-04 13:22:53 -0300

    Refactor events and add start cut button

---
+++ src/components/ConfirmationModal/ConfirmationModal.tsx
@@ -0,0 +1,79 @@
+import React, { Dispatch } from 'react';
+import { Modal, Button, Typography, Box, Paper } from '@material-ui/core';
+import { Close } from '@material-ui/icons';
+import useStyles from './confirmation-modal-styles';
+
+interface ConfirmationModalProps {
+  ceramicSize: string;
+  opened: boolean;
+  setOpened: Dispatch<boolean>;
+  confirmAction: () => void;
+}
+
+export const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+  opened,
+  setOpened,
+  ceramicSize,
+  confirmAction,
+}) => {
+  const classes = useStyles();
+
+  const closeModal = () => {
+    setOpened(false);
+  };
+
+  const getModalStyle = () => {
+    const top = 50;
+    const left = 50;
+
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  };
+
+  if (!opened) return null;
+
+  return (
+    <Modal open={opened} onClose={closeModal}>
+      <Paper style={getModalStyle()} className={classes.paper}>
+        <Box className={classes.header}>
+          <Typography variant="h5">Início de corte</Typography>
+
+          <Close className={classes.closeIcon} onClick={closeModal} />
+        </Box>
+
+        <Box className={classes.content}>
+          <Typography variant="subtitle1">
+            Certifique-se que a cerâmica com as dimensões corretas (
+            {ceramicSize}) está posicionada corretamente na máquina.
+            <br />
+            <br />
+            Deseja realmente iniciar o corte?
+          </Typography>
+        </Box>
+
+        <Box className={classes.actions}>
+          <Button
+            variant="contained"
+            color="secondary"
+            style={{ marginRight: 16 }}
+            onClick={() => {
+              confirmAction();
+              closeModal();
+            }}
+          >
+            Sim
+          </Button>
+
+          <Button variant="contained" color="primary" onClick={closeModal}>
+            Cancelar
+          </Button>
+        </Box>
+      </Paper>
+    </Modal>
+  );
+};
+
+export default ConfirmationModal;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,41 +8,72 @@ import {
   CardMedia,
   Typography,
   CardContent,
+  Button,
+  Tooltip,
 } from '@material-ui/core';
 import useStyles from './home-styles';
 import { base64ToUrl } from '../utils/image';
 import { PanicModal } from '../components/PanicModal';
 import { Ceramic } from '../types/Ceramic';
+import { ConfirmationModal } from '../components/ConfirmationModal/ConfirmationModal';
 
-const REMOTE_SERVER = 'http://localhost:3001';
+const REMOTE_SERVER = 'http://localhost:3005';
 const socket = io(REMOTE_SERVER);
 
 export const Home: React.FC = () => {
   const classes = useStyles();
 
+  const [errorMessage, setErrorMessage] = useState('');
   const [panicModalOpened, setPanicModalOpened] = useState(false);
+  const [isConnected, setIsConnected] = useState(socket.connected);
   const [ceramicList, setCeramicList] = useState<Ceramic[]>([] as Ceramic[]);
-  useEffect(() => {
-    socket.on('connect', () => console.log(`Connected to ${REMOTE_SERVER} ..`));
-    const updateList = (newCeramic: Ceramic) =>
-      setCeramicList([...ceramicList, newCeramic]);
-
-    socket.on('ceramicList', updateList);
-    console.log(ceramicList, typeof ceramicList);
-    return () => {
-      socket.off('connect');
-      socket.off('ceramicList');
-    };
-  }, []);
-
-  const cancelCut = () => {
-    console.log('Cancelamento selecionado');
-  };
+  const [confirmationModalOpened, setConfirmationModalOpened] = useState(false);
 
   const restartCut = () => {
-    console.log('Reiniciamento selecionado');
+    return true;
   };
-  console.log(ceramicList);
+
+  const cancelCut = () => {
+    return true;
+  };
+
+  const updateList = (data: string) => {
+    try {
+      const newCeramicList: Ceramic[] = JSON.parse(data);
+
+      setCeramicList(newCeramicList);
+    } catch (error) {
+      console.error(error);
+
+      setErrorMessage('Ocorreu um erro ao buscar os cortes');
+    }
+  };
+
+  const startCut = () => {
+    socket.emit('start-cut', JSON.stringify(ceramicList[0]));
+  };
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+
+      socket.emit('get-state');
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('state', updateList);
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('state');
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Container>
       <PanicModal
@@ -52,72 +83,134 @@ export const Home: React.FC = () => {
         restartCut={restartCut}
       />
 
-      <Typography gutterBottom variant="h5" style={{ margin: '16px 0' }}>
-        Fila de cortes
-      </Typography>
+      <ConfirmationModal
+        opened={confirmationModalOpened}
+        setOpened={setConfirmationModalOpened}
+        ceramicSize={`${ceramicList[0]?.width}x${ceramicList[0]?.height}`}
+        confirmAction={startCut}
+      />
+
+      <Box
+        style={{
+          marginTop: 16,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
+      >
+        <Box style={{ display: 'flex', flexDirection: 'column' }}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            style={{
+              display: 'flex',
+              margin: '16px 8px',
+              alignItems: 'center',
+            }}
+          >
+            Fila de cortes
+            <Tooltip
+              title={
+                isConnected ? 'Servidor conectado' : 'Servidor desconectado'
+              }
+            >
+              <div
+                style={{
+                  width: 16,
+                  height: 16,
+                  marginLeft: 16,
+                  borderRadius: '50%',
+                  backgroundColor: isConnected ? 'green' : 'red',
+                }}
+              />
+            </Tooltip>
+          </Typography>
+        </Box>
+
+        <Button
+          color="primary"
+          variant="contained"
+          disabled={!ceramicList.length}
+          style={{ height: 40 }}
+          onClick={() => setConfirmationModalOpened(true)}
+        >
+          Iniciar corte
+        </Button>
+      </Box>
 
       <Box className={classes.listContainer}>
-        {ceramicList.map((ceramic) => (
-          <Badge
-            key={ceramic.order}
-            badgeContent={`#${ceramic.order}`}
-            color="primary"
+        {errorMessage ? (
+          <Typography
+            variant="body2"
+            color="secondary"
+            style={{ margin: '8px 0 16px 0' }}
           >
-            <Card className={classes.ceramicCard}>
-              <CardMedia
-                component="img"
-                alt="Imagem corte"
-                title="Imagem corte"
-                image={ceramic.cutImage}
-                className={classes.cutImage}
-              />
+            {errorMessage}
+          </Typography>
+        ) : (
+          ceramicList.map((ceramic, idx) => (
+            <Badge
+              // eslint-disable-next-line react/no-array-index-key
+              key={`ceramic-${idx}`}
+              badgeContent={`#${idx + 1}`}
+              color="primary"
+            >
+              <Card className={classes.ceramicCard}>
+                <CardMedia
+                  component="img"
+                  alt="Imagem corte"
+                  title="Imagem corte"
+                  image={base64ToUrl(ceramic.base64)}
+                  className={classes.cutImage}
+                />
 
-              <hr />
+                <hr />
 
-              <CardContent style={{ padding: 0 }}>
-                <Typography gutterBottom variant="h6" component="h2">
-                  Informações cerâmica
-                </Typography>
+                <CardContent style={{ padding: 0 }}>
+                  <Typography gutterBottom variant="h6" component="h2">
+                    Informações cerâmica
+                  </Typography>
 
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  style={{ margin: '8px 0' }}
-                >
-                  <strong>Largura:</strong> {ceramic.length} cm
-                </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    style={{ margin: '8px 0' }}
+                  >
+                    <strong>Comprimento:</strong> {ceramic.width} cm
+                  </Typography>
 
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  style={{ margin: '8px 0' }}
-                >
-                  <strong>Comprimento:</strong> {ceramic.width} cm
-                </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    style={{ margin: '8px 0' }}
+                  >
+                    <strong>Largura:</strong> {ceramic.height} cm
+                  </Typography>
 
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  style={{ margin: '8px 0' }}
-                >
-                  <strong>Espessura:</strong> {ceramic.depth} cm
-                </Typography>
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    style={{ margin: '8px 0' }}
+                  >
+                    <strong>Espessura:</strong> {ceramic.depth} cm
+                  </Typography>
 
-                <Typography
-                  variant="body2"
-                  color="textSecondary"
-                  component="p"
-                  style={{ margin: '8px 0' }}
-                >
-                  <strong>Repetições:</strong> {ceramic.repetitions}
-                </Typography>
-              </CardContent>
-            </Card>
-          </Badge>
-        ))}
+                  <Typography
+                    variant="body2"
+                    color="textSecondary"
+                    component="p"
+                    style={{ margin: '8px 0' }}
+                  >
+                    <strong>Repetições:</strong> {ceramic.repetitions}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Badge>
+          ))
+        )}
       </Box>
     </Container>
   );
